69c55afc52aa7f1b9460d05a4df17c877b47d669
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-27 16:49:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,23 @@
-def my_each # put argument(s) here
-  # code here
+an_array = [1, 2, 3, 4]
+
+def my_each(array)
+  i = 0
+  while (i <= 4) 
+    puts array[i]
+    # yield(item)
+  end
 end
+
+my_each(an_array) do 
+  # puts item 
+  i += 1
+end
+
+
+# def yielding
+#   puts "the program is executing the code inside the method"
+#   yield
+#   puts "now we are back in the method"
+# end
+
+# yielding { puts "the method has yielded to the block!" }
