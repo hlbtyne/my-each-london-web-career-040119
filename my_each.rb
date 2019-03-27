f78531e3098560e847b8f4bbcf86c052285982e6
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-27 16:53:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,17 +2,15 @@ an_array = [1, 2, 3, 4]
 
 def my_each(array)
   i = 0
-  while (i <= 4) 
-    puts array[i]
+  while (i <= 4)
+    number = array[i]
+    yield(number)
     i += 1 
-    # yield(item)
   end
 end
 
-my_each(an_array) do 
-  # puts item 
-  i += 1
-end
+puts number
+
 
 
 # def yielding
