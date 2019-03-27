4169349bca1bb01c0ccc9d64935b17da1d4448cf
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-27 16:56:06 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -13,10 +13,10 @@ my_each(an_array) { puts number }
 
 
 
-def yielding_with_arguments(num)
-  puts "the program is executing the code inside the method"
-  yield(num)
-  puts "now we are back in the method"
-end
+# def yielding_with_arguments(num)
+#   puts "the program is executing the code inside the method"
+#   yield(num)
+#   puts "now we are back in the method"
+# end
 
-yielding_with_arguments(2) {|i| puts i * 2}
+# yielding_with_arguments(2) {|i| puts i * 2}
